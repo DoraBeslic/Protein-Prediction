adda35015199a1953049ce81043282e40870376b
DoraBeslic <beslic.i@northeastern.edu>
2023-04-07 10:46:13 -0400

    added script to run hmmscan to find protein domains

---
+++ scripts/pfamScan_args.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# pfamScan_args.sh
+# Usage: bash scripts/pfamScan_args.sh <domtblout> <Pfam-A> <longest orfs> 1>results/logs/pfamScan_args.log 2>results/logs/pfamScan_args.err
+
+hmmscan --cpu 4 --domtblout $1 $2 $3
